7728668a23ad32446795724894139ef1ec45d964
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2019-04-09 21:30:32 +0300

    Added MainViewCell, wip

---
+++ hackernews-stories/classes/modules/main/view/cell/MainCellView.swift
@@ -0,0 +1,21 @@
+//
+//  MainCellView.swift
+//  hackernews-stories
+//
+//  Created by Alexander Tereshkov on 4/9/19.
+//  Copyright © 2019 Alexander Tereshkov. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+}
